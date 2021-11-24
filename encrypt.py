5d4c0e124e84c1a300db2b0b8b9eceaaf9efab86
stefan <diaconustefan16@gmail.com>
2021-11-24 12:06:29 +0200

    mere???

---
+++ encrypt.py
@@ -0,0 +1,12 @@
+def enc(bytesOfLenKeyPls, keyPls):
+
+    #pad cu 0
+    if len(bytesOfLenKeyPls) < len(keyPls):
+        bytesOfLenKeyPls = '0' * (len(keyPls)-len(bytesOfLenKeyPls))+bytesOfLenKeyPls
+            
+    for i in ['0'*(10-len(bin(ord(a) ^ ord(b))))+bin(ord(a) ^ ord(b))[2:] for a, b in zip(bytesOfLenKeyPls, keyPls)]:
+        print(i)
+        
+
+
+
