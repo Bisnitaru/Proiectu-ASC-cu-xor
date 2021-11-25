38687ac685489535fd21f85186bd5d49537422cd
stefan <diaconustefan16@gmail.com>
2021-11-25 16:35:52 +0200

    am gresit la padding :((

--- encrypt.py
+++ encrypt.py
@@ -2,7 +2,7 @@ def enc(bytesOfLenKeyPls, keyPls):
 
     #pad cu 0
     if len(bytesOfLenKeyPls) < len(keyPls):
-        bytesOfLenKeyPls = '0' * (len(keyPls)-len(bytesOfLenKeyPls))+bytesOfLenKeyPls
+        bytesOfLenKeyPls = '\0' * (len(keyPls)-len(bytesOfLenKeyPls))+bytesOfLenKeyPls
             
     for i in ['0'*(10-len(bin(ord(a) ^ ord(b))))+bin(ord(a) ^ ord(b))[2:] for a, b in zip(bytesOfLenKeyPls, keyPls)]:
         print(i)
