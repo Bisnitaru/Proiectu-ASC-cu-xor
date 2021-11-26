d37eb8628117588aa01f0dd067801ea3d7a731e7
StefanMagureanu25 <82400321+StefanMagureanu25@users.noreply.github.com>
2021-11-26 15:56:28 +0200

    Am pus encrypt-ul din main

--- encrypt.py
+++ encrypt.py
@@ -1,11 +1,12 @@
-def enc(bytesOfLenKeyPls, keyPls):
-
-    #pad cu 0
+def encrypt(bytesOfLenKeyPls, keyPls):
+    encrypted_text = ""
     if len(bytesOfLenKeyPls) < len(keyPls):
-        bytesOfLenKeyPls = '\0' * (len(keyPls)-len(bytesOfLenKeyPls))+bytesOfLenKeyPls
-            
-    for i in ['0'*(10-len(bin(ord(a) ^ ord(b))))+bin(ord(a) ^ ord(b))[2:] for a, b in zip(bytesOfLenKeyPls, keyPls)]:
-        print(i)
+        bytesOfLenKeyPls = '\0' * (len(keyPls) - len(bytesOfLenKeyPls)) + bytesOfLenKeyPls
+        
+    for i in ['0' * (10 - len(bin(ord(a) ^ ord(b)))) + bin(ord(a) ^ ord(b))[2:] for a, b in zip(bytesOfLenKeyPls, keyPls)] 
+        encrypted_text += i
+        
+    return encrypted_text
         
 
 
