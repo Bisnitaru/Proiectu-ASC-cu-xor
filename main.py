64f695b07593661617b6de4906adde48a2780d0f
StefanMagureanu25 <82400321+StefanMagureanu25@users.noreply.github.com>
2021-11-26 11:59:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from textwrap import wrap
+
+
 def encrypt(bytesOfLenKeyPls, keyPls):
     encrypted_text = ""
     if len(bytesOfLenKeyPls) < len(keyPls):
@@ -5,13 +8,13 @@ def encrypt(bytesOfLenKeyPls, keyPls):
 
     for i in ['0' * (10 - len(bin(ord(a) ^ ord(b)))) + bin(ord(a) ^ ord(b))[2:] for a, b in
               zip(bytesOfLenKeyPls, keyPls)]:
-        encrypted_text += i + ' '
+        encrypted_text += i
     return encrypted_text
 
 
 def decrypt(bytesOfLenKey, key):
     decrypted_text = ""
-    for x, y in zip(bytesOfLenKey.split(), key.split()):
+    for x, y in zip(wrap(bytesOfLenKey, 8), wrap(key, 8)):
         character = (bin(int(x, base=2) ^ int(y, base=2))[2::])
         if chr(int(character, base=2)) != '\0':
             decrypted_text += chr(int(character, base=2))
@@ -31,13 +34,13 @@ decrypted_text = ""
 for ch in key:
     if (len(bin(ord(ch))[2::]) < 8):
         encrypted_key += '0' * (8 - len(bin(ord(ch))[2::]))
-    encrypted_key += bin(ord(ch))[2::] + ' '
+    encrypted_key += bin(ord(ch))[2::]
 while text_length > 0:
     encrypted_text += encrypt(text_to_encrypt, key)
     text_to_encrypt = text_to_encrypt[key_length::]
     text_length -= key_length
 print("Cheia criptata este:", encrypted_key)
-print("Textul criptat in binar este: ", encrypted_text)
+print("Textul criptat in binar este:", encrypted_text)
 encrypted_text_length = len(encrypted_text)
 encrypted_key_length = len(encrypted_key)
 while encrypted_text_length > 0:
