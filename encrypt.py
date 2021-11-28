1e3274383deb5bbbf18a73480ea608a17210392e
stefan <diaconustefan16@gmail.com>
2021-11-28 17:34:54 +0200

    mare greseala camarazi

--- encrypt.py
+++ encrypt.py
@@ -4,9 +4,11 @@ def enc(bytesOfLenKeyPls, keyPls):
     if len(bytesOfLenKeyPls) < len(keyPls):
         bytesOfLenKeyPls = '\0' * (len(keyPls)-len(bytesOfLenKeyPls))+bytesOfLenKeyPls
             
-    for i in ['0'*(10-len(bin(ord(a) ^ ord(b))))+bin(ord(a) ^ ord(b))[2:] for a, b in zip(bytesOfLenKeyPls, keyPls)]:
-        print(i)
-        
+    for i in [(ord(a) ^ ord(b)) for a, b in zip(bytesOfLenKeyPls, keyPls)]:
+        print(i.to_bytes(1,'big'))
+        #Fișierul input.txt este mereu unul text. Fișierul output este unul binar, nu text (conține același număr de caractere ca și input.txt).
+        #nu e binar in sensu ala
 
+        
 
 
