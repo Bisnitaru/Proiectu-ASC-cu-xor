9de633f08061e8b8bf38255e56c2120c8c496524
stefan <diaconustefan16@gmail.com>
2021-12-01 10:31:35 +0200

    ca o referinta

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,20 @@
-def encrypt(bytesOfLenKeyPls, keyPls):
-    encrypted_text = ""
-    if len(bytesOfLenKeyPls) < len(keyPls):
-        bytesOfLenKeyPls = '\0' * (len(keyPls)-len(bytesOfLenKeyPls))+bytesOfLenKeyPls
+f=open('out','wb')
+g=open('input.txt','rb')
 
-    for i in [(ord(a) ^ ord(b)) for a, b in zip(bytesOfLenKeyPls, keyPls)]:
-        print(i.to_bytes(1,'big'))
+def encrypt(bytearrayCuBytesLenKeyPls, keyPls):
+    if len(bytearrayCuBytesLenKeyPls) < len(keyPls):
+        bytearrayCuBytesLenKeyPls = bytearray('\0' * (len(keyPls)-len(bytearrayCuBytesLenKeyPls)),encoding='UTF-8')+bytearrayCuBytesLenKeyPls
+    print(bytearrayCuBytesLenKeyPls)
+    for i in [(a ^ ord(b)) for a, b in zip(bytearrayCuBytesLenKeyPls , keyPls)]:
+        f.write(i.to_bytes(1, 'big'))
         #Fișierul input.txt este mereu unul text. Fișierul output este unul binar, nu text (conține același număr de caractere ca și input.txt).
         #nu e binar in sensu ala
 
 
-        
-
-
+while 1:
+    b=bytearray(g.read(10))
+    if b != b'':
+        encrypt(b, '4RyAZ9aJ7m')
+            
+    else:
+        break
