fee4b81636e6b134306b3102dcef3e8218eccc81
StefanMagureanu25 <82400321+StefanMagureanu25@users.noreply.github.com>
2021-12-02 19:18:37 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,18 +1,14 @@
-f=open('out','wb')
+f=open('output.txt','wb')
 g=open('input.txt','rb')
 
 def encrypt(bytearrayCuBytesLenKeyPls, keyPls):
-    # print(bytearrayCuBytesLenKeyPls)
     for i in [(a ^ ord(b)) for a, b in zip(bytearrayCuBytesLenKeyPls , keyPls)]:
         f.write(i.to_bytes(1, 'big'))
-        #Fișierul input.txt este mereu unul text. Fișierul output este unul binar, nu text (conține același număr de caractere ca și input.txt).
-        #nu e binar in sensu ala
 
 
 while 1:
     b=bytearray(g.read(10))
     if b != b'':
-        encrypt(b, 'key???')
-            
+        encrypt(b, 'key???')      
     else:
         break
