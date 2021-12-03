6bf48b507ac729a7a319fd0506a004e1a3e728a2
stefan <diaconustefan16@gmail.com>
2021-12-03 10:45:40 +0200

    mere?

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,17 @@
-f=open('output.txt','wb')
-g=open('input.txt','rb')
+import sys
+f=open(sys.argv[3],'wb')
+g=open(sys.argv[2],'rb')
 
 def encrypt(bytearrayCuBytesLenKeyPls, keyPls):
+    print(bytearrayCuBytesLenKeyPls)
     for i in [(a ^ ord(b)) for a, b in zip(bytearrayCuBytesLenKeyPls , keyPls)]:
         f.write(i.to_bytes(1, 'big'))
 
 
 while 1:
-    b=bytearray(g.read(10))
+    b=bytearray(g.read(len(sys.argv[1])))
     if b != b'':
-        encrypt(b, 'key???')      
+        encrypt(b, sys.argv[1])
+            
     else:
         break
